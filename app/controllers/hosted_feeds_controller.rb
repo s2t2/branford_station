a1b987a2fa795740d3e87bba090a1545dd7312b4
MJ Rossetti (@s2t2) <s2t2mail@gmail.com>
2015-04-12 18:08:28 -0700

    wip

--- app/controllers/hosted_feeds_controller.rb
+++ app/controllers/hosted_feeds_controller.rb
@@ -18,4 +18,12 @@ class HostedFeedsController < ApplicationController
     latest_version = feed.latest_version
     redirect_to hosted_feed_version_path(:version_id => latest_version.id)
   end
+
+  def check
+    feed_id = params[:feed_id]
+    feed = Feed.find(feed_id)
+    feed.check_for_updates
+    flash[:info] = "Checking feed source for new content. This may take a moment. Refresh the page for results."
+    #redirect_to hosted_feed_path(:feed_id => feed.id)
+  end
 end
