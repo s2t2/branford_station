133dc5b07d9b737c3121e18826394ba513eb64e6
MJ Rossetti (@s2t2) <s2t2mail@gmail.com>
2015-04-13 22:42:56 -0700

    editing

--- app/controllers/stop_versions_controller.rb
+++ app/controllers/stop_versions_controller.rb
@@ -1,8 +1,8 @@
 class StopVersionsController < ApplicationController
-	def index
-		version_id = params[:version_id]
+  def index
+    version_id = params[:version_id]
     @stops = StopVersion.where(:version_id => version_id)
-	end
+  end
 
   #def show
   #
