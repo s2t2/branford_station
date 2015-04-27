779c7c321926198bc0865368dda93cee01019bd5
MJ Rossetti (@s2t2) <s2t2mail@gmail.com>
2015-04-26 17:36:26 -0700

    fixing redirect

--- app/controllers/stop_versions_controller.rb
+++ app/controllers/stop_versions_controller.rb
@@ -2,7 +2,15 @@ class StopVersionsController < ApplicationController
   def index
     version_id = params[:version_id]
     @stops = StopVersion.where(:version_id => version_id)
-    #redirect_to hosted_feed_version_path(:version_id => version_id)
+
+    respond_to do |format|
+      format.html {
+        redirect_to hosted_feed_version_path(:version_id => version_id)
+      }
+      format.json {
+        render json: @stops
+      }
+    end
   end
 
   def show
